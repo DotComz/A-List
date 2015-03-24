0b6fe4d6524e227ba4947197430ac698a634c7ad
Michael Lasso <lassomicha@hotmail.com>
2015-03-23 20:57:30 -0400

    Linked web/endpoints, added welcome/landing pages

--- endpoints-alist/src/main/java/com/dotcomz/alist/Constants.java
+++ endpoints-alist/src/main/java/com/dotcomz/alist/Constants.java
@@ -4,7 +4,7 @@ package com.dotcomz.alist;
  * Contains the client IDs and scopes for allowed clients consuming the helloworld API.
  */
 public class Constants {
-  public static final String WEB_CLIENT_ID = "760301236458-utaopf3hcv1qc9ah9aihb00cc0avcb29.apps.googleusercontent.com";
+  public static final String WEB_CLIENT_ID = "1083260449954-knkapobd9h0jjvfbd2ept422t1k7lc8g.apps.googleusercontent.com";
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
